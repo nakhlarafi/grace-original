e4c12231d387e0004ad69e37d5d1832267560c2a
nakhlarafi <nakhla054@gmail.com>
2023-04-24 17:08:11 -0400

    new embedding size

--- run_grace.sh
+++ run_grace.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
-
-# Run the Python script with the argument 'Math'
-python3 runtotal.py Math
+#!/bin/bash
+#SBATCH -J Grace --mem=120G --account=r_mdnakh
+#SBATCH --gpus=20gb:1
+module load anaconda/3.2022.10
+python3 runtotal.py Lang
