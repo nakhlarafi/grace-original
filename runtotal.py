f98764545dfa5eb682ae946e57d2eb361a7709a2
nakhlarafi <nakhla054@gmail.com>
2023-06-15 12:20:35 -0400

    gat added

--- runtotal.py
+++ runtotal.py
@@ -6,7 +6,7 @@ import pickle
 project = sys.argv[1]
 card = [1, 2, 3]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
-singlenums = {'Time':5, 'Math':2, "Lang":1, "Chart":3, "Mockito":1, "Closure":1}
+singlenums = {'Time':5, 'Math':2, "Lang":1, "Chart":3, "Mockito":3, "Closure":1}
 singlenum = singlenums[project]
 totalnum = len(card) * singlenum
 lr = 1e-2
