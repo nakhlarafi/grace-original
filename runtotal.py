618eb50608f4876802c82d836582873edb53515b
nakhlarafi <nakhla054@gmail.com>
2023-06-16 15:39:29 -0400

    gat added

--- runtotal.py
+++ runtotal.py
@@ -4,14 +4,14 @@ import time
 import os, sys
 import pickle
 project = sys.argv[1]
-card = [1,2,3]
+card = [0]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
 singlenums = {'Time':1, 'Math':2, "Lang":1, "Chart":3, "Mockito":1, "Closure":1}
 singlenum = singlenums[project]
 totalnum = len(card) * singlenum
 lr = 1e-2
 seed = 0
-batch_size = 1
+batch_size = 10
 for i in tqdm(range(int(len(lst) / totalnum) + 1)):
     jobs = []
     for j in range(totalnum):
