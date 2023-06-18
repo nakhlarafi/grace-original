7bd26edd3f197056ba63d35fc5a5738afc9e1381
nakhlarafi <nakhla054@gmail.com>
2023-06-18 16:51:38 -0400

    gat added

--- runtotal.py
+++ runtotal.py
@@ -4,7 +4,7 @@ import time
 import os, sys
 import pickle
 project = sys.argv[1]
-card = [0,1]
+card = [0]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
 singlenums = {'Time':5, 'Math':2, "Lang":10, "Chart":3, "Mockito":6, "Closure":1}
 singlenum = singlenums[project]
@@ -18,7 +18,7 @@ for i in tqdm(range(int(len(lst) / totalnum) + 1)):
         if totalnum * i + j >= len(lst):
             continue
         cardn =int(j / singlenum)
-        p = subprocess.Popen("CUDA_VISIBLE_DEVICES="+str(card[cardn]) + " python run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
+        p = subprocess.Popen("CUDA_VISIBLE_DEVICES=0,1 python run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
         jobs.append(p)
         time.sleep(10)
     for p in jobs:
