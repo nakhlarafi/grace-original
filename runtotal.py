3b9cad5f3167e64f3e1fa501deba56c2c050e98c
nakhlarafi <nakhla054@gmail.com>
2023-06-20 12:28:26 -0400

    data distributed parallel feature try

--- runtotal.py
+++ runtotal.py
@@ -19,7 +19,7 @@ for i in tqdm(range(int(len(lst) / totalnum) + 1)):
         if totalnum * i + j >= len(lst):
             continue
         cardn =int(j / singlenum)
-        p = subprocess.Popen("CUDA_VISIBLE_DEVICES=1,2" + " python3 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
+        p = subprocess.Popen("CUDA_VISIBLE_DEVICES=0,1" + " python3 run.py %d %s %f %d %d"%(lst[totalnum * i + j], project, lr, seed, batch_size), shell=True)
         p.wait()
 
 p = subprocess.Popen("python3 sum.py %s %d %f %d"%(project, seed, lr, batch_size), shell=True)
