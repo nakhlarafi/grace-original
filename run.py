afa5debb72207b84c304902249fb6d1cdfac7b21
nakhlarafi <nakhla054@gmail.com>
2023-06-29 21:23:54 -0400

    mapping dim change

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ class dotdict(dict):
     def __getattr__(self, name):
         return self[name]
 
-NlLen_map = {"Time":200, "Math":4500, "Lang":280, "Chart": 2350, "Mockito":1780, "unknown":2200}
+NlLen_map = {"Time":500, "Math":4500, "Lang":280, "Chart": 2350, "Mockito":1780, "unknown":2200}
 CodeLen_map = {"Time":1300, "Math":2700, "Lang":300, "Chart":5250, "Mockito":1176, "unknown":2800}
 args = dotdict({
     'NlLen':NlLen_map[sys.argv[2]],
