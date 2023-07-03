85f5dbc982788c9303df768bf4c277a9cbb9be89
nakhlarafi <nakhla054@gmail.com>
2023-07-02 21:06:37 -0400

    changed the eps and the epoch

--- runtotal.py
+++ runtotal.py
@@ -6,7 +6,7 @@ import pickle
 project = sys.argv[1]
 card = [0]
 lst = list(range(len(pickle.load(open(project + '.pkl', 'rb')))))
-singlenums = {'Time':5, 'Math':2, "Lang":10, "Chart":3, "Mockito":4, "Closure":1}
+singlenums = {'Time':5, 'Math':2, "Lang":1, "Chart":3, "Mockito":4, "Closure":1}
 singlenum = singlenums[project]
 totalnum = len(card) * singlenum
 lr = 1e-2
