87d3f951d6fe364625c2acffc5b83fe810395e3a
nakhlarafi <nakhla054@gmail.com>
2023-07-03 22:06:45 -0400

    added line count metric

--- Model.py
+++ Model.py
@@ -18,7 +18,7 @@ class NlEncoder( nn.Module ):
         self.feed_forward_hidden = 4 * self.embedding_size
         self.conv = nn.Conv2d( self.embedding_size, self.embedding_size, (1, self.word_len) )
         self.transformerBlocks = nn.ModuleList(
-            [TransformerBlock( self.embedding_size, 2, self.feed_forward_hidden, 0.1 ) for _ in range(5)] )
+            [TransformerBlock( self.embedding_size, 2, self.feed_forward_hidden, 0.1 ) for _ in range(3)] )
         self.token_embedding = nn.Embedding( args.Nl_Vocsize, self.embedding_size - 1 )
         self.token_embedding1 = nn.Embedding( args.Nl_Vocsize, self.embedding_size - 1 )
 
@@ -45,6 +45,8 @@ class NlEncoder( nn.Module ):
         lineem = torch.cat([lineem, linemus_norm.unsqueeze(-1).float()], dim=-1)
         
         x = torch.cat( [x, lineem], dim=1 )
+        print('x', x.shape)
+        print('linetype', linetype.shape)
         check = 0
         for trans in self.transformerBlocks:
             check+=1
