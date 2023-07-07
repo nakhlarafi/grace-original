d5c82da85ea519f2ad813f8a164a396cb7330e30
nakhlarafi <nakhla054@gmail.com>
2023-07-07 11:04:59 -0400

    the mmc feature added

--- run.py
+++ run.py
@@ -19,8 +19,8 @@ class dotdict(dict):
     def __getattr__(self, name):
         return self[name]
 
-NlLen_map = {"Time":3900, "Math":4500, "Lang":300, "Chart": 1000, "Mockito":800, "unknown":2200}
-CodeLen_map = {"Time":1300, "Math":2700, "Lang":300, "Chart":5250, "Mockito":1100, "unknown":2800}
+NlLen_map = {"Time":3900, "Math":4500, "Lang":300, "Chart": 1000, "Mockito":700, "unknown":2200}
+CodeLen_map = {"Time":1300, "Math":2700, "Lang":300, "Chart":5250, "Mockito":1176, "unknown":2800}
 args = dotdict({
     'NlLen':NlLen_map[sys.argv[2]],
     'CodeLen':CodeLen_map[sys.argv[2]],
