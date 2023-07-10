5cd5d3e23a321ae96128b4100d592fee2dfe0a21
nakhlarafi <nakhla054@gmail.com>
2023-07-10 19:50:03 -0400

    changed the nmlen map to 200x300 lang

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ class dotdict(dict):
         return self[name]
 
 NlLen_map = {"Time":2600, "Math":4500, "Lang":50, "Chart": 1000, "Mockito":650, "unknown":2200}
-CodeLen_map = {"Time":1300, "Math":2700, "Lang":100, "Chart":5250, "Mockito":1176, "unknown":2800}
+CodeLen_map = {"Time":1300, "Math":2700, "Lang":300, "Chart":5250, "Mockito":1176, "unknown":2800}
 args = dotdict({
     'NlLen':NlLen_map[sys.argv[2]],
     'CodeLen':CodeLen_map[sys.argv[2]],
