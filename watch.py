95084b3c540220ef33cdc32268079c95e96b0f1c
nakhlarafi <nakhla054@gmail.com>
2023-07-18 18:06:00 -0400

    first commit

--- watch.py
+++ watch.py
@@ -104,66 +104,10 @@ for x in score:
 # print(len(best_ids))
 
 
-# best_epoch = sorted(eps.items(), key=lambda x:x[1])[-1][0]
-# top1 = 0
-# top3 = 0
-# top5 = 0
-# mfr = []
-# mar = []
-# for idx in p:
-#     xs = p[idx]
-#     each_epoch_pred = xs[3]
-#     best_pred = each_epoch_pred[best_epoch]
-#     score_pred = each_epoch_pred[str(best_epoch)+'_pred']
-#     print('-'*20)
-#     print('Project Number:', idx)
-#     print('Correct Answer:', f[idx]['ans'])
-#     print(best_pred)
-#     print(score_pred)
-#     ar = []
-#     minl = 1e9
-#     to1 = 0
-#     to3 = 0
-#     to5 = 0
-#     for x in f[idx]['ans']:
-#         m = best_pred.index(x)
-#         ar.append(m)
-#         minl = min(minl, m)
-#     if minl == 0:
-#         top1 += 1
-#         to1 = 1
-#     if minl < 3:
-#         top3 += 1
-#         to3 = 1
-#     if minl < 5:
-#         top5 += 1
-#         to5 = 1
-#     mfr.append(minl)
-#     mar.append(np.mean(ar))
-#     print('Top1:', to1)
-#     print('Top3:', to3)
-#     print('Top5:', to5)
-#     print('-'*20)
-# result_path = os.path.join("result-all")
-# if not os.path.exists(result_path):
-#     os.makedirs(result_path)
-
-# print('-----------------------------')
-# print('top1:',top1)
-# print('top3:',top3)
-# print('top5:',top5)
-# print('mfr:',np.mean(mfr))
-# print('mar:',np.mean(mar))
-# print('-----------------------------')
-
-# with open(result_path + '/' + pr, 'w') as f:
-#     f.write('top1: %d\n'%top1)
-#     f.write('top3: %d\n'%top3)
-#     f.write('top5: %d\n'%top5)
-#     f.write('mfr: %f\n'%np.mean(mfr))
-#     f.write('mar: %f\n'%np.mean(mar))
 best_epoch = sorted(eps.items(), key=lambda x:x[1])[-1][0]
-top_count = [0] * 5  # list to count correct items in each position
+top1 = 0
+top3 = 0
+top5 = 0
 mfr = []
 mar = []
 for idx in p:
@@ -174,8 +118,64 @@ for idx in p:
     print('-'*20)
     print('Project Number:', idx)
     print('Correct Answer:', f[idx]['ans'])
-    print('Ranking positions:',best_pred)
-    print('Scores:',score_pred)
+    print(best_pred)
+    print(score_pred)
+    ar = []
+    minl = 1e9
+    to1 = 0
+    to3 = 0
+    to5 = 0
+    for x in f[idx]['ans']:
+        m = best_pred.index(x)
+        ar.append(m)
+        minl = min(minl, m)
+    if minl == 0:
+        top1 += 1
+        to1 = 1
+    if minl < 3:
+        top3 += 1
+        to3 = 1
+    if minl < 5:
+        top5 += 1
+        to5 = 1
+    mfr.append(minl)
+    mar.append(np.mean(ar))
+    print('Top1:', to1)
+    print('Top3:', to3)
+    print('Top5:', to5)
+    print('-'*20)
+result_path = os.path.join("result-all")
+if not os.path.exists(result_path):
+    os.makedirs(result_path)
+
+print('------------Original-----------------')
+print('top1:',top1)
+print('top3:',top3)
+print('top5:',top5)
+print('mfr:',np.mean(mfr))
+print('mar:',np.mean(mar))
+print('-----------------------------')
+
+# with open(result_path + '/' + pr, 'w') as f:
+#     f.write('top1: %d\n'%top1)
+#     f.write('top3: %d\n'%top3)
+#     f.write('top5: %d\n'%top5)
+#     f.write('mfr: %f\n'%np.mean(mfr))
+#     f.write('mar: %f\n'%np.mean(mar))
+# best_epoch = sorted(eps.items(), key=lambda x:x[1])[-1][0]
+top_count = [0] * 5  # list to count correct items in each position
+mfr = []
+mar = []
+for idx in p:
+    xs = p[idx]
+    each_epoch_pred = xs[3]
+    best_pred = each_epoch_pred[best_epoch]
+    score_pred = each_epoch_pred[str(best_epoch)+'_pred']
+    # print('-'*20)
+    # print('Project Number:', idx)
+    # print('Correct Answer:', f[idx]['ans'])
+    # print('Ranking positions:',best_pred)
+    # print('Scores:',score_pred)
     ar = []
     minl = 1e9
     for x in f[idx]['ans']:
@@ -192,16 +192,16 @@ for idx in p:
     top3 = sum(top_count[:3])
     top5 = sum(top_count)
 
-    print('Current Top1:', top1)
-    print('Current Top3:', top3)
-    print('Current Top5:', top5)
-    print('-'*20)
+    # print('Current Top1:', top1)
+    # print('Current Top3:', top3)
+    # print('Current Top5:', top5)
+    # print('-'*20)
 
-result_path = os.path.join("result-all")
-if not os.path.exists(result_path):
-    os.makedirs(result_path)
+# result_path = os.path.join("result-all")
+# if not os.path.exists(result_path):
+#     os.makedirs(result_path)
 
-print('-----------------------------')
+print('------------GBMFL-----------------')
 print('Final top1:',top1)
 print('Final top3:',top3)
 print('Final top5:',top5)
@@ -209,9 +209,9 @@ print('mfr:',np.mean(mfr))
 print('mar:',np.mean(mar))
 print('-----------------------------')
 
-with open(result_path + '/' + pr, 'w') as f:
-    f.write('top1: %d\n' % top1)
-    f.write('top3: %d\n' % top3)
-    f.write('top5: %d\n' % top5)
-    f.write('mfr: %f\n' % np.mean(mfr))
-    f.write('mar: %f\n' % np.mean(mar))
+# with open(result_path + '/' + pr, 'w') as f:
+#     f.write('top1: %d\n' % top1)
+#     f.write('top3: %d\n' % top3)
+#     f.write('top5: %d\n' % top5)
+#     f.write('mfr: %f\n' % np.mean(mfr))
+#     f.write('mar: %f\n' % np.mean(mar))
